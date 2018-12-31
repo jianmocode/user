91687ca8aa9fad61cdaae292165927c754650058
Max <zhuce@diancloud.com>
2018-12-31 18:15:32 +0800

    + mark 方法

--- api/Checkin.php
+++ api/Checkin.php
@@ -28,6 +28,63 @@ class Checkin extends Api {
 	 * 自定义函数 
 	 */
 
+    // @KEEP BEGIN
+
+
+    function mark($query, $data ) {
+
+        $u = new \Xpmsns\User\Model\User;
+        $user = $u->getUserInfo();
+        $user_id = $user["user_id"];
+
+        if ( empty($user_id) ) {
+            throw new Excp("用户尚未登录", 402, ["query"=>$query, "data"=>$data]);
+        }
+
+        $time = time();
+        $ci = new \Xpmsns\User\Model\Checkin;
+
+        // 签到请求清洗
+        if ( $data["limit"] === "hourly") {  // 按小时
+            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-d H:i:s', $time-3600)]);
+            if ( $rows["total"] > 0 ) {
+                throw new Excp("已经签过了, 一小时后再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
+            }
+
+        } elseif ( $data["limit"] === "daily") { // 按天
+            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date("Y-m-d 00:00:00")]);
+            if ( $rows["total"] > 0 ) {
+                throw new Excp("今天已经签过了, 明天再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
+            }
+
+        } elseif ( $data["limit"] === "weekly") { // 按周
+            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-d 00:00:00', strtotime('monday this week'))]);
+            if ( $rows["total"] > 0 ) {
+                throw new Excp("本周已经签过了, 下周再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
+            }
+
+        } elseif ( $data["limit"] === "monthly_limit") { // 按月
+            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-01 00:00:00')]);
+            if ( $rows["total"] > 0 ) {
+                throw new Excp("本月已经签过了, 下月再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
+            }
+        }
+
+        // 处理特别数据
+        if ( !empty($data["data"]) ) {
+            $data["data"] = Utils::json_decode($data["data"]);
+        }
+
+        $data["user_id"] = $user_id;
+        $data["time"] = date('Y-m-d H:i:s', $time);
+
+        return $ci->create( $data );
+
+
+    }
+
+
+    // @KEEP END
 
 
 
