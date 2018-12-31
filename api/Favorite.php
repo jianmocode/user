ff3b560be7bd065616ed7333fca284a102b2bd3f
Max <zhuce@diancloud.com>
2018-12-31 19:21:31 +0800

    + 收藏API

--- api/Favorite.php
+++ api/Favorite.php
@@ -29,11 +29,101 @@ class Favorite extends Api {
 	 */
 
 
+    // @KEEP BEGIN
+
+    /**
+     * 添加资源或地址到收藏夹
+     * @method POST /_api/xpmsns/user/favorite/create
+     */
+    function create($query, $data ) {
+
+        $u = new \Xpmsns\User\Model\User;
+        $user = $u->getUserInfo();
+        $user_id = $user["user_id"];
+
+        if ( empty($user_id) ) {
+            throw new Excp("用户尚未登录", 402, ["query"=>$query, "data"=>$data]);
+        }
+
+        $time = time();
+        $fav = new \Xpmsns\User\Model\Favorite;
+        $data["user_id"] = $user_id;
+        try {
+            $resp =  $fav->create( $data );
+        } catch( Excp $e ) {
+            if ( $e->getCode() == 1062 ) {
+                throw new Excp("你已经收藏过了", 1062, ["user_id"=>$user_id, "data"=>$data]);
+            }
+            throw $e;
+        }
+
+        return $resp;
+
+    }
+
+
+    /**
+     * 移除收藏记录
+     * @method POST /_api/xpmsns/user/favorite/create
+     */
+    function remove($query, $data ) {
+
+
+        if (empty($data["favorite_id"])){
+            throw new Excp("未指定收藏记录", 402, ["query"=>$query, "data"=>$data]);
+        }
+
+        $u = new \Xpmsns\User\Model\User;
+        $user = $u->getUserInfo();
+        $user_id = $user["user_id"];
+
+        if ( empty($user_id) ) {
+            throw new Excp("用户尚未登录", 402, ["query"=>$query, "data"=>$data]);
+        }
+
+        $time = time();
+        $fav = new \Xpmsns\User\Model\Favorite;
+        $favorite_id = $data["favorite_id"];
+
+        $favorite = $fav->getByFavoriteId($favorite_id);
+        if ( $favorite["user_id"] != $user_id ) {
+            throw new Excp("您没有该收藏的删除权限", 403, ["user_id"=>$user_id, "data"=>$data]);
+        }
+
+       $resp = $fav->remove($favorite_id, "favorite_id");
+
+       if ( $resp == true ) {
+           return ["code"=>0, "message"=>"移除收藏成功"];
+       }
+
+       throw new Excp("移除收藏失败", 500, ["query"=>$query, "data"=>$data]);
+
+    }
+
+
+    /**
+     * 查询收藏记录
+     * @method GET /_api/xpmsns/user/favorite/search
+     * @see \Xpmsns\User\Model\Favorite
+     */
+    function search( $query, $data ) {
+        $u = new \Xpmsns\User\Model\User;
+        $user = $u->getUserInfo();
+        $user_id = $user["user_id"];
+
+        if ( empty($user_id) ) {
+            throw new Excp("用户尚未登录", 402, ["query"=>$query, "data"=>$data]);
+        }
+
+        $query["user_id"] = $user_id;
+        $fav = new \Xpmsns\User\Model\Favorite;
+        return $rows = $fav->search( $query );
+
+    }
 
 
 
-
-
+    // @KEEP END
 
 
 
