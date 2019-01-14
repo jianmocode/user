5bcc1195131e3229ec1577be4abd2a021660a4c7
Max <zhuce@diancloud.com>
2019-01-14 10:12:58 +0800

    fix checkin bug

--- api/Checkin.php
+++ api/Checkin.php
@@ -50,25 +50,25 @@ class Checkin extends Api {
 
         // 签到请求清洗
         if ( $data["limit"] === "hourly") {  // 按小时
-            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-d H:i:s', $time-3600)]);
+            $rows = $ci->search(["user_user_id"=>$user_id, "time_after"=>date('Y-m-d H:i:s', $time-3600)]);
             if ( $rows["total"] > 0 ) {
                 throw new Excp("已经签过了, 一小时后再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
             }
 
         } elseif ( $data["limit"] === "daily") { // 按天
-            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date("Y-m-d 00:00:00")]);
+            $rows = $ci->search(["user_user_id"=>$user_id, "time_after"=>date("Y-m-d 00:00:00")]);
             if ( $rows["total"] > 0 ) {
                 throw new Excp("今天已经签过了, 明天再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
             }
 
         } elseif ( $data["limit"] === "weekly") { // 按周
-            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-d 00:00:00', strtotime('monday this week'))]);
+            $rows = $ci->search(["user_user_id"=>$user_id, "time_after"=>date('Y-m-d 00:00:00', strtotime('monday this week'))]);
             if ( $rows["total"] > 0 ) {
                 throw new Excp("本周已经签过了, 下周再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
             }
 
         } elseif ( $data["limit"] === "monthly") { // 按月
-            $rows = $ci->search(["user_id"=>$user_id, "time_after"=>date('Y-m-01 00:00:00')]);
+            $rows = $ci->search(["user_user_id"=>$user_id, "time_after"=>date('Y-m-01 00:00:00')]);
             if ( $rows["total"] > 0 ) {
                 throw new Excp("本月已经签过了, 下月再试.", 403, ["rows"=>$rows, "user_id"=>$user_id]);
             }
