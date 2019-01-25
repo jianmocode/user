e6dc00ab7b350b49a26ad44077051f15a0579106
Max <zhuce@diancloud.com>
2019-01-26 00:40:41 +0800

    debug

--- model/Checkin.php
+++ model/Checkin.php
@@ -158,6 +158,7 @@ class Checkin extends Model {
             $process = $i + 1;
 
             $job->info( "\thistory: user_id={$ci["user_id"]} time={$ci["time"]} process={$process} ");
+            $job->info( "\t---\n" . json_encode($ci) . "\n---" );
 
             // 历史时间对比
             $curr = strtotime(date("Y-m-d 00:00:00", strtotime($ci["time"])));
